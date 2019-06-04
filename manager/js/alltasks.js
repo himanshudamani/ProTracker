42258b6b4ab7b7d468014ea56601d0d568c6655d
himanshudamani <himanshuvdamani@gmail.com>
2019-06-04 16:05:28 +0530

    alltasks table updated

---
+++ manager/js/alltasks.js
@@ -0,0 +1,37 @@
+//Select Project Dropdown
+var selectProInnerHtml="<option value='--select--'>--Select--</option>";
+selectProInnerHtml += JSON.parse(localStorage.projects).map(
+  project => `<option value=${project.name}>${project.name}</option>`
+);
+
+document.getElementById('selectPro').innerHTML = selectProInnerHtml;
+
+//Table Display
+function DisplayTable(){
+
+var getTasks =JSON.parse(localStorage.getItem("tasks")) ;
+var selectValue = document.getElementById('selectPro').value;
+var  tasksTableInnerHtml;
+tasksTableInnerHtml="";
+
+if (selectValue== '--select--') {
+  document.getElementById('tasksTable').innerHTML= "<label>Select a project please</label>";
+}
+
+for (var i = 0; i < getTasks.length; i++) {
+if (getTasks[i].projectname == selectValue) {
+
+  tasksTableInnerHtml+="<tr>";
+  tasksTableInnerHtml += "<td>" + getTasks[i].name + "</td>";
+  tasksTableInnerHtml += "<td>" + getTasks[i].type + "</td>";
+  tasksTableInnerHtml += "<td>" + getTasks[i].atatus + "</td>";
+  tasksTableInnerHtml += "<td>" + getTasks[i].assignedTo + "</td>";
+  tasksTableInnerHtml += "<td>" + getTasks[i].orighrs + "</td>";
+  tasksTableInnerHtml += "<td>" + getTasks[i].type + "</td></tr>";
+
+  document.getElementById('tasksTable').innerHTML = tasksTableInnerHtml;
+}
+
+}
+
+}
