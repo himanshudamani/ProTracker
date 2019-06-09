4a53153a28ed41114dda8dd2fea2baabd696fdac
himanshudamani <himanshuvdamani@gmail.com>
2019-06-09 15:41:21 +0530

    modal edit button

--- manager/js/addtask.js
+++ manager/js/addtask.js
@@ -29,7 +29,7 @@ document.getElementById('tProInput').innerHTML = ProInputinnerHtml;
     projectname: tPro,
     assignedTo: tAssignedTo,
     orighrs: orighours,
-    workedhrs: '0';
+    workedhrs: '0',
     type: tType,
     date: date
   };
